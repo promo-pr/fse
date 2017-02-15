f77d4b0832f74c57c8d2d60bac8da26d5157ff89
Александр <S!ta!rik@hota@bch@77@>
2017-02-15 14:58:06 +0400

    module event

---
+++ migrations/m170215_104518_creste_post_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\db\Migration;
+
+class m170215_104518_creste_post_table extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%post}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string()->notNull(),
+            'body' => $this->text(),
+            'slug' => $this->string(),
+            'seotitle' => $this->string(),
+            'keywords' => $this->string(),
+            'description' => $this->text(),
+            'created_at' => $this->integer(),
+            'updated_at' => $this->integer(),
+            'status' => $this->smallInteger()->notNull()->defaultValue(1),
+        ], $tableOptions);
+
+        $this->createIndex('idx-post-slug', '{{%post}}', 'slug');
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%page}}');
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
